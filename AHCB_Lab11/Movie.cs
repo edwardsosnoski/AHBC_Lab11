4f7311193ae345f026e2bffb07b3c737076108a8
edwardsosnoski <edward.sosnoski@gmail.com>
2019-08-12 18:31:55 -0400

    first commit

---
+++ AHCB_Lab11/Movie.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AHCB_Lab11
+{
+    public class Movie
+    {
+        private string Title;
+        private string Category;
+
+        public void NewMovie(string title, string category)
+        {
+            Title = title;
+            Category = category;
+        }
+
+        public string GetCategory(string messageToUser, string errorMessage, List<string> categories)
+        {
+            var validInput = false;
+            string category = "";
+
+            do
+            {
+                Console.WriteLine(messageToUser);
+                var userInput = Console.ReadLine();
+
+                if (!categories.Contains(userInput))
+                {
+                    Console.WriteLine(errorMessage);
+                }
+                else
+                {
+                    category = userInput;
+                }
+            } while (validInput);
+            
+            return category;
+        }
+
+        public void GetMoviesOfCategory(string category,List<Movie> moviesList)
+        {
+            bool continuePrompt = true;
+            do
+            {
+                foreach (Movie movie in moviesList)
+                {
+                    if (category.Equals(movie.Category))
+                    {
+                        Console.WriteLine(movie.Title);
+                    }
+                }
+                Console.WriteLine("Continue? y/n");
+                string userInput = Console.ReadLine();
+
+                if (userInput.Equals("n"))
+                {
+                    continuePrompt = false;
+                }
+            } while (continuePrompt); 
+        }
+    }
+}
---
+++ AHCB_Lab11/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AHCB_Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var movie1 = new Movie();
+            movie1.NewMovie("Howl's Moving Castle", "Animated");
+
+            var movie2 = new Movie();
+            movie2.NewMovie("Wolf Children", "Animated");
+
+            var movie3 = new Movie();
+            movie3.NewMovie("Atonment", "Drama");
+
+            var movie4 = new Movie();
+            movie4.NewMovie("The Notebook", "Drama");
+
+            var movie5 = new Movie();
+            movie5.NewMovie("Inception", "Sci-Fi");
+
+            var movie6 = new Movie();
+            movie6.NewMovie("The Hills Have Eyes", "Horror");
+
+            var movie7 = new Movie();
+            movie7.NewMovie("Cabin Fever", "Horror");
+
+            var movie8 = new Movie();
+            movie8.NewMovie("Star Wars Episode II", "Sci-Fi");
+
+            var movie9 = new Movie();
+            movie9.NewMovie("Summer Wars", "Animated");
+
+            var movie10 = new Movie();
+            movie10.NewMovie("Titanic", "Drama");
+
+            var categories = new List<string>()
+            {
+                "Animated",
+                "Drama",
+                "Sci-Fi",
+                "Horror"
+            };
+
+            var listOfMovies = new List<Movie>()
+            {
+                movie1, movie2, movie3, movie4, movie5, movie6, movie7, movie8, movie9, movie10
+            };
+
+            string userInput = movie1.GetCategory(
+                "What category are you interested in? Select Drama, Sci-Fi, Animated, or Horror:",
+                "That is not a valid entry.",
+                categories);
+
+            movie1.GetMoviesOfCategory(userInput, listOfMovies);
+        }
+    }
+}
